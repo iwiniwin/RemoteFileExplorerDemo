0d26335bcba314a0bcd7f66f468e86bf9649d8bf
iwiniwin <iwiniwin@foxmail.com>
2021-11-22 01:18:23 +0800

    添加Remote File Explorer

--- Assets/StreamingAssets/changetextcontent.lua
+++ Assets/StreamingAssets/changetextcontent.lua
@@ -1,3 +1,3 @@
 local obj = CS.UnityEngine.Object.FindObjectOfType(typeof(CS.Game.ChangeTextColor))
 local textComponent = obj:GetComponent(typeof(CS.UnityEngine.UI.Text));
-textComponent.text = "Hello, World"
+textComponent.text = "Hello, Wlrod"
